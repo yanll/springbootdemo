1d0c750ce0604081ab792d11ab59c9a1705c969b
yanll <m3GAGGM7>
2018-03-13 18:08:45 +0800

    商户等级增加本地缓存

--- src/main/java/com/cmp/study/springdemo/web/IndexController.java
+++ src/main/java/com/cmp/study/springdemo/web/IndexController.java
@@ -4,6 +4,8 @@ import com.cmp.study.springdemo.service.IIndexService;
 import com.cmp.study.springdemo.service.IUserService;
 import com.cmp.study.springdemo.vo.UserVO;
 import com.yeepay.g3.facade.bankinfo.dto.AccInfoDTO;
+import com.yeepay.g3.facade.bankinfo.dto.HeadBankDTO;
+import com.yeepay.g3.facade.bankinfo.service.AccInfoMgrFacade;
 import com.yeepay.g3.facade.bankinfo.service.BankInfoQueryFacade;
 import com.yeepay.g3.utils.rmi.RemoteServiceFactory;
 import com.yeepay.g3.utils.rmi.RemotingProtocol;
@@ -27,18 +29,29 @@ public class IndexController {
 
 
     @RequestMapping(value = "/test")
-    public AccInfoDTO test() {
-        String url = "http://127.0.0.1:8086/bankinfo-hessian/hessian";
+    public Object test() {
+        //ConfigParam<?> configParam = ConfigurationUtils.getConfigParam("config_type_text_resources", "bankinfo-hessian-redis-switch");
+        //Object obj = configParam.getValue();
+        //if (true) return obj;
+        //String url = "http://127.0.0.1:8086/bankinfo-hessian/hessian";
         if (true) {
-            //AccInfoMgrFacade p = RemoteServiceFactory.getService(url, RemotingProtocol.HESSIAN, AccInfoMgrFacade.class);
-            //p.printEnv();
-            BankInfoQueryFacade p = RemoteServiceFactory.getService(url, RemotingProtocol.HESSIAN, BankInfoQueryFacade.class);
+            AccInfoMgrFacade p = RemoteServiceFactory.getService(RemotingProtocol.HESSIAN, AccInfoMgrFacade.class);
+            p.printEnv();
+        }
+        if (false) {
+            BankInfoQueryFacade p = RemoteServiceFactory.getService(RemotingProtocol.HESSIAN, BankInfoQueryFacade.class);
+            if (true) return p.queryAllProvince();
             AccInfoDTO acc = new AccInfoDTO();
             acc.setAccNo("622599033244222");
             acc.setBranchBankNo("303791000193");
             acc.setBranchBankName("中国光大银行西安新城支行");
             acc = p.studyAccInfoOne(acc);
             return acc;
+        }
+        if (true) {
+            BankInfoQueryFacade p = RemoteServiceFactory.getService(RemotingProtocol.HESSIAN, BankInfoQueryFacade.class);
+            HeadBankDTO headbank = p.queryHeadBankByCode("CCB");
+            return headbank;
         }
         return null;
     }
