5d3d87c438d990113c07cef8bb63d92941c1f546
yanll <m3GAGGM7>
2018-03-15 11:34:18 +0800

    增加日志记录

--- src/main/java/com/cmp/study/springdemo/web/IndexController.java
+++ src/main/java/com/cmp/study/springdemo/web/IndexController.java
@@ -3,12 +3,6 @@ package com.cmp.study.springdemo.web;
 import com.cmp.study.springdemo.service.IIndexService;
 import com.cmp.study.springdemo.service.IUserService;
 import com.cmp.study.springdemo.vo.UserVO;
-import com.yeepay.g3.facade.bankinfo.dto.AccInfoDTO;
-import com.yeepay.g3.facade.bankinfo.dto.HeadBankDTO;
-import com.yeepay.g3.facade.bankinfo.service.AccInfoMgrFacade;
-import com.yeepay.g3.facade.bankinfo.service.BankInfoQueryFacade;
-import com.yeepay.g3.utils.rmi.RemoteServiceFactory;
-import com.yeepay.g3.utils.rmi.RemotingProtocol;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,35 +20,6 @@ public class IndexController {
 
     @Autowired
     IUserService userService;
-
-
-    @RequestMapping(value = "/test")
-    public Object test() {
-        //ConfigParam<?> configParam = ConfigurationUtils.getConfigParam("config_type_text_resources", "bankinfo-hessian-redis-switch");
-        //Object obj = configParam.getValue();
-        //if (true) return obj;
-        //String url = "http://127.0.0.1:8086/bankinfo-hessian/hessian";
-        if (true) {
-            AccInfoMgrFacade p = RemoteServiceFactory.getService(RemotingProtocol.HESSIAN, AccInfoMgrFacade.class);
-            p.printEnv();
-        }
-        if (false) {
-            BankInfoQueryFacade p = RemoteServiceFactory.getService(RemotingProtocol.HESSIAN, BankInfoQueryFacade.class);
-            if (true) return p.queryAllProvince();
-            AccInfoDTO acc = new AccInfoDTO();
-            acc.setAccNo("622599033244222");
-            acc.setBranchBankNo("303791000193");
-            acc.setBranchBankName("中国光大银行西安新城支行");
-            acc = p.studyAccInfoOne(acc);
-            return acc;
-        }
-        if (true) {
-            BankInfoQueryFacade p = RemoteServiceFactory.getService(RemotingProtocol.HESSIAN, BankInfoQueryFacade.class);
-            HeadBankDTO headbank = p.queryHeadBankByCode("CCB");
-            return headbank;
-        }
-        return null;
-    }
 
 
     @RequestMapping(value = "/hello")
