649ab4e387163a7e45cc323480273318c9e54a21
yanll <liangliang.yan@yeepay.com>
2019-06-11 18:36:21 +0800

    BOSS commit

--- src/main/java/tk/techforge/springdemo/web/HomeController.java
+++ src/main/java/tk/techforge/springdemo/web/HomeController.java
@@ -7,39 +7,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import tk.techforge.springdemo.entity.HomeVO;
-import tk.techforge.springdemo.entity.ResponseMsg;
 
 /**
  * Created by YANLL on 2016/03/30.
  */
 
-@Api(value = "主页API", tags = {"主页","ALL"})
+@Api(value = "主页API", tags = {"主页", "ALL"})
 @RestController
 @RequestMapping
 @Slf4j
 public class HomeController {
 
-    @ApiOperation("SSS")
+    @ApiOperation("主页")
     @RequestMapping(value = "/home", method = RequestMethod.GET)
     public HomeVO home() {
         HomeVO homeVO = new HomeVO();
         homeVO.setName("hello");
         return homeVO;
-    }
-
-    @ApiOperation("SSS")
-    @RequestMapping(value = "/homeT", method = RequestMethod.GET)
-    public ResponseMsg<HomeVO> homeT() {
-        HomeVO homeVO = new HomeVO();
-        homeVO.setName("hello,world!");
-        return ResponseMsg.<HomeVO>instance(homeVO);
-    }
-
-    @ApiOperation("SSS")
-    @RequestMapping(value = "/home_", method = RequestMethod.GET)
-    public ResponseMsg home_() {
-        HomeVO homeVO = new HomeVO();
-        homeVO.setName("hello,boy!");
-        return ResponseMsg.instance(homeVO);
     }
 }
