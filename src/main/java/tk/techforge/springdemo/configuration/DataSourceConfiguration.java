2f001feb57c4a92767a5c3362fa79864ad9e2c54
yanll <liangliang.yan@yeepay.com>
2020-07-03 18:42:22 +0800

    commit push

--- src/main/java/tk/techforge/springdemo/configuration/DataSourceConfiguration.java
+++ src/main/java/tk/techforge/springdemo/configuration/DataSourceConfiguration.java
@@ -1,7 +1,7 @@
 package tk.techforge.springdemo.configuration;
 
-import com.alibaba.druid.pool.DruidDataSource;
 import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.boot.autoconfigure.SpringBootVFS;
@@ -34,20 +34,18 @@ public class DataSourceConfiguration {
     @Bean(name = "dataSource")
     public DataSource dataSource() throws SQLException {
         log.info("开始初始化数据源");
-        DruidDataSource datasource = new DruidDataSource();
-        datasource.setUrl(env.getProperty("spring.datasource.url"));
-        datasource.setUsername(env.getProperty("spring.datasource.username"));
-        datasource.setPassword(env.getProperty("spring.datasource.password"));
-        datasource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
-        datasource.setMinIdle(5);
-        datasource.setMaxActive(20);
-        datasource.setTestWhileIdle(true);
-        datasource.setTestOnBorrow(false);
-        datasource.setTestOnReturn(false);
-        datasource.setTimeBetweenEvictionRunsMillis(60);
-        datasource.setValidationQuery("select 'x'");
-        datasource.init();
-        return datasource;
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(env.getProperty("spring.datasource.url"));
+        dataSource.setUsername(env.getProperty("spring.datasource.username"));
+        dataSource.setPassword(env.getProperty("spring.datasource.password"));
+        dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+        dataSource.setMinimumIdle(5);
+        dataSource.setMaximumPoolSize(20);
+        dataSource.setMaxLifetime(20);
+        dataSource.setPoolName("HikariDataSourcePool");
+        dataSource.setConnectionInitSql("select 'x'");
+        dataSource.getConnection();
+        return dataSource;
     }
 
 
