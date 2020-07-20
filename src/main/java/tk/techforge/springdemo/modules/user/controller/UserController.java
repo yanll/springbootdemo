989bd0ab411568d5d7da76a5de035cb062db4488
yanll <liangliang.yan@yeepay.com>
2020-07-20 16:37:57 +0800

    commit push

--- src/main/java/tk/techforge/springdemo/modules/user/controller/UserController.java
+++ src/main/java/tk/techforge/springdemo/modules/user/controller/UserController.java
@@ -2,10 +2,10 @@ package tk.techforge.springdemo.modules.user.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import tk.techforge.springdemo.commons.PaginationUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import tk.techforge.springdemo.commons.PaginationUtil;
 import tk.techforge.springdemo.commons.ResponseMsg;
 import tk.techforge.springdemo.modules.user.bean.User;
 import tk.techforge.springdemo.modules.user.service.UserService;
@@ -14,6 +14,8 @@ import java.io.Serializable;
 
 ;
 ;
+
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
